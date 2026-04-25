f7b73849b858251bc185e90b2e894c2f4928fac4
Bamahfoudh <m91919174@gmail.com>
2026-04-25 18:59:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,39 @@
 import requests
+from bs4 import BeautifulSoup
 
 BOT_TOKEN = "8761813650:AAFtvKLzkHzMBgelkLhcY-7sWHcTVVFYsGA"
 CHAT_ID = "1849103"
 
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+hashtags = [
+    "صدر_حديثًا",
+    "صدر_حديثا",
+    "جديد_الكتب"
+]
 
-requests.post(url, data={
-    "chat_id": CHAT_ID,
-    "text": "تم الإرسال بنجاح ✅"
-})
+for tag in hashtags:
+    url = f"https://nitter.net/search?q=%23{tag}&f=images"
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    images = soup.find_all("img")
+
+    count = 0
+    for img in images:
+        src = img.get("src")
+
+        if src and "pic" in src:
+            img_url = "https://nitter.net" + src
+
+            requests.post(
+                f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto",
+                data={
+                    "chat_id": CHAT_ID,
+                    "photo": img_url,
+                    "caption": f"#{tag}"
+                }
+            )
+
+            count += 1
+            if count == 3:
+                break
