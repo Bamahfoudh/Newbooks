0964f5e26c85eefa8a57e32d41c03eb7f1fcb5bc
Bamahfoudh <m91919174@gmail.com>
2026-04-25 18:48:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,9 @@ import requests
 BOT_TOKEN = "8761813650:AAFtvKLzkHzMBgelkLhcY-7sWHcTVVFYsGA"
 CHAT_ID = "1849103"
 
-requests.post(
-    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-    data={"chat_id": CHAT_ID, "text": "اشتغل البوت ✅"}
-)
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": "تم الإرسال بنجاح ✅"
+})
