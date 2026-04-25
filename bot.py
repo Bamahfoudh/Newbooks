d85245fe332d5b1f8a3f3a9a7bdcab1405f70288
Bamahfoudh <m91919174@gmail.com>
2026-04-25 19:39:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import requests
-from bs4 import BeautifulSoup
 
 BOT_TOKEN = "8761813650:AAFtvKLzkHzMBgelkLhcY-7sWHcTVVFYsGA"
 CHAT_ID = "1849103"
@@ -17,53 +16,10 @@ def send_message(text):
         "text": text
     })
 
-def send_photo(photo_url, caption):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto"
-    requests.post(url, data={
-        "chat_id": CHAT_ID,
-        "photo": photo_url,
-        "caption": caption
-    })
-
-def get_tweets(tag):
-    url = f"https://nitter.poast.org/search?q=%23{tag}&f=live"
-    r = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
-    soup = BeautifulSoup(r.text, "html.parser")
-
-    tweets = []
-
-    for item in soup.select(".timeline-item")[:3]:
-        text_tag = item.select_one(".tweet-content")
-        img_tag = item.select_one("img")
-
-        if not text_tag:
-            continue
-
-        text = text_tag.text.strip()
-
-        img_url = None
-        if img_tag and img_tag.get("src") and "profile_images" not in img_tag["src"]:
-            img_url = "https://nitter.poast.org" + img_tag["src"]
-
-        tweets.append((text, img_url))
-
-    return tweets
-
-
 def main():
     for tag in hashtags:
-        send_message(f"📚 #{tag}")
-
-        tweets = get_tweets(tag)
-
-        if tweets:
-            for text, img in tweets:
-                if img:
-                    send_photo(img, text)
-                else:
-                    send_message(text)
-        else:
-            send_message("ما لقيت نتائج (المصدر ممكن معطل)")
+        url = f"https://twitter.com/search?q=%23{tag}&src=typed_query&f=live"
+        send_message(f"📚 #{tag}\n{url}")
 
 if __name__ == "__main__":
     main()
